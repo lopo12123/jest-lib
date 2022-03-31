57cc0b5ca83841f0fcf75ddee8efa1bdc084a118
ChenMH <2036604698@qq.com>
2022-03-31 21:56:45 +0800

    daily: 2022-03-31 #728

--- src/lk.test.ts
+++ src/lk.test.ts
@@ -1,12 +1,5 @@
 import { lk } from "./lk"
 
 test('test sum', () => {
-    expect(lk(123123)).toBe(false)
-    expect(lk(12123)).toBe(false)
-    expect(lk(1)).toBe(true)
-    expect(lk(0)).toBe(true)
-    expect(lk(-1)).toBe(false)
-    expect(lk(102)).toBe(false)
-    expect(lk(121)).toBe(true)
-    expect(lk(123321)).toBe(true)
+    expect(lk(1, 22)).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22])
 })
--- src/lk.ts
+++ src/lk.ts
@@ -1,16 +1,32 @@
-function lk(x: number): boolean {
-    if(x < 0) return false
-    else {
-        const xStr = x.toString()
-        for(let i = 0; i < xStr.length / 2; i ++) {
-            if(xStr[i] !== xStr[xStr.length - 1 - i]) return false
+function lk(left: number, right: number): number[] {
+    const judge = (n: number): boolean => {
+        if(n < 9) return true
+        const copyN = n
+        const nums: number[] = []
+        while (n >= 1) {
+            if(n % 10 === 0) return false
+            nums.push(n % 10)
+            n = Math.floor(n / 10)
+        }
+
+        if([ ...new Set(nums) ].length === 1) return true
+
+        for (let i = 0; i < nums.length; i++) {
+            if(copyN % nums[i] !== 0) {
+                return false
+            }
         }
         return true
     }
+
+    const res: number[] = []
+    for (let i = left; i < right + 1; i++) {
+        if(judge(i)) res.push(i)
+    }
+
+    return res
 }
 
 export {
     lk
 }
-
-console.log(lk(11))  // -1 1
