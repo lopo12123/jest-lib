cf9a486b6bed80a9e937c6c8edc63916945235a4
ChenMH <2036604698@qq.com>
2022-04-03 14:38:35 +0800

    daily: 2022-04-03 #14

--- src/lk.ts
+++ src/lk.ts
@@ -1,26 +1,18 @@
-function lk(s: string): number {
-    let sum = 0
-    for(let i = 0; i < s.length; i ++) {
-        if(s[i] === 'I') sum += (s[i + 1] === 'V' || s[i + 1] === 'X') ? -1 : 1
-        else if(s[i] === 'X') sum += (s[i + 1] === 'L' || s[i + 1] === 'C') ? -10 : 10
-        else if(s[i] === 'C') sum += (s[i + 1] === 'D' || s[i + 1] === 'M') ? -100 : 100
-        else {
-            switch (s[i]) {
-                case 'V':
-                    sum += 5
-                    break
-                case 'L':
-                    sum += 50
-                    break
-                case 'D':
-                    sum += 500
-                    break
-                case 'M':
-                    sum += 1000
+function lk(strs: string[]): string {
+    const prefix: string[] = []
+
+    let columnChar: string
+
+    for(let i = 0; i < strs[0].length; i ++) {
+        columnChar = strs[0][i]
+        for(let j = 0; j < strs.length; j ++) {
+            if(strs[j][i] === undefined || strs[j][i] !== columnChar) {
+                return prefix.join('')
             }
         }
+        prefix.push(columnChar)
     }
-    return sum
+    return prefix.join('')
 }
 
 export {
