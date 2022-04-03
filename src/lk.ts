528e025a52868a43ceabab6f3c36d8e4325583c6
ChenMH <2036604698@qq.com>
2022-04-03 14:52:23 +0800

    daily: 2022-04-03 #20

--- src/lk.ts
+++ src/lk.ts
@@ -1,22 +1,32 @@
-function lk(strs: string[]): string {
-    const prefix: string[] = []
+function lk(s: string): boolean {
+    const toPush: {[k: string]: number} =  {
+        '(': 1,
+        '[': 2,
+        '{': 3
+    }
+    const toPop: {[k: string]: number} = {
+        '}': -3,
+        ']': -2,
+        ')': -1
+    }
+    const stack: number[] = []
 
-    let columnChar: string
-
-    for(let i = 0; i < strs[0].length; i ++) {
-        columnChar = strs[0][i]
-        for(let j = 0; j < strs.length; j ++) {
-            if(strs[j][i] === undefined || strs[j][i] !== columnChar) {
-                return prefix.join('')
+    for(let i = 0; i < s.length; i ++) {
+        if(toPush[s[i]]) stack.push(toPush[s[i]])
+        else if(toPop[s[i]]) {
+            if((toPop[s[i]] + stack[stack.length - 1]) !== 0) return false
+            else {
+                stack.pop()
             }
         }
-        prefix.push(columnChar)
     }
-    return prefix.join('')
+    return stack.length === 0
 }
 
 export {
     lk
 }
+
+console.log(lk('()'))
 
 
