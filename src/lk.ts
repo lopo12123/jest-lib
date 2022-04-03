6784373fbb5c9a8c4967bd1e8764c5d4356b45ad
ChenMH <2036604698@qq.com>
2022-04-03 14:22:06 +0800

    daily: 2022-04-03 #13

--- src/lk.ts
+++ src/lk.ts
@@ -1,9 +1,26 @@
-// @ts-ignore
-function lk(letters: string[], target: string): string {
-    if(target >= letters[letters.length - 1]) return letters[0]
-    for(let i = 0; i < letters.length; i ++) {
-        if(letters[i] > target) return letters[i]
+function lk(s: string): number {
+    let sum = 0
+    for(let i = 0; i < s.length; i ++) {
+        if(s[i] === 'I') sum += (s[i + 1] === 'V' || s[i + 1] === 'X') ? -1 : 1
+        else if(s[i] === 'X') sum += (s[i + 1] === 'L' || s[i + 1] === 'C') ? -10 : 10
+        else if(s[i] === 'C') sum += (s[i + 1] === 'D' || s[i + 1] === 'M') ? -100 : 100
+        else {
+            switch (s[i]) {
+                case 'V':
+                    sum += 5
+                    break
+                case 'L':
+                    sum += 50
+                    break
+                case 'D':
+                    sum += 500
+                    break
+                case 'M':
+                    sum += 1000
+            }
+        }
     }
+    return sum
 }
 
 export {
