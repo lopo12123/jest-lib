5750fa0aff2fe0ade1146358f1947f0b93d1c57a
ChenMH <2036604698@qq.com>
2022-04-07 00:05:33 +0800

    daily: 2022-04-07

--- src/lk.test.ts
+++ src/lk.test.ts
@@ -1,7 +1,7 @@
 import { lk } from "./lk"
 
 test('test sum', () => {
-    expect(lk(["zyx","wvu","tsr"])).toBe(3)
-    expect(lk(["xc","yb","za"])).toBe(0)
-    expect(lk(["ca","bb","ac"])).toBe(1)
+    expect(lk([ 1, 3, 5, 6 ], 5)).toBe(2)
+    expect(lk([ 1, 3, 5, 6 ], 2)).toBe(1)
+    expect(lk([ 1, 3, 5, 6 ], 7)).toBe(4)
 })
--- src/lk.ts
+++ src/lk.ts
@@ -1,19 +1,28 @@
-function lk(nums: number[], val: number): number {
-    let sumIndex = 0
-    for(let i = 0; i < nums.length; i ++) {
-        if(nums[i] !== val) {
-            nums[sumIndex++] = nums[i]
+function lk(nums: number[], target: number): number {
+    if(target <= nums[0]) return 0
+    else if(target > nums[nums.length - 1]) return nums.length
+
+    let place = 0
+    let scope = nums
+
+    while (scope.length > 1) {
+        const partLength = Math.floor(scope.length / 2)
+        if(scope[partLength-1] < target && target <= scope[partLength]) return place + partLength
+        else if(scope[partLength - 1] === target) return place + partLength - 1
+        else if(target < scope[partLength - 1]) scope.splice(partLength)
+        else if(target > scope[partLength]) {
+            place += partLength
+            scope.splice(0, partLength)
         }
     }
-    return sumIndex  // index + 1 === length
+    return place + 1
 }
 
 export {
     lk
 }
 
-const ori = [3,2,2,3]
-console.log(lk(ori, 3), ori)
+console.log(lk([1,3,5,6], 5))
 
 
 
