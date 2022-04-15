36cc51eb8b62ff99ed3c21454e5570809f9cd976
lopo <lopo@zju.edu.cn>
2022-04-15 23:07:58 +0800

    daily: 2022-04-15 #232

--- src/lk.ts
+++ src/lk.ts
@@ -8,17 +8,17 @@
 //     }
 // }
 
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.left = (left === undefined ? null : left)
-        this.right = (right === undefined ? null : right)
-    }
-}
+// class TreeNode {
+//     val: number
+//     left: TreeNode | null
+//     right: TreeNode | null
+//
+//     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//         this.val = (val === undefined ? 0 : val)
+//         this.left = (left === undefined ? null : left)
+//         this.right = (right === undefined ? null : right)
+//     }
+// }
 
 // class NestedInteger {
 //     #list: (number | NestedInteger) [] = []
@@ -52,43 +52,86 @@ class TreeNode {
 //     }
 // }
 
+class MyQueue {
+    #store: number[] = []
+    #assist: number[] = []
 
-function lk(nums: number[]): string[] {
-    if(nums.length === 0) return []
+    constructor() {
 
-    const res: string[] = []
+    }
 
-    // 从第一个开始直接计数
-    let start: number = nums[0]
+    push(x: number): void {
+        this.#store.push(x)
+    }
 
-    const last = nums.reduce((prev, curr) => {
-        // 已经开始计数 且遇到终点
-        if(curr - prev !== 1) {
-            // 只计了一个数
-            if(prev === start) {
-                res.push(start + '')
-            }
-            // 计了多个数
-            else {
-                res.push(`${start}->${prev}`)
-            }
-            start = curr
+    pop(): number {
+        for (let i = this.#store.length - 1; i >= 0; i--) {
+            this.#assist.push(this.#store.pop()!)
         }
-        return curr
-    })
-
-    if(start === last) {
-        res.push(start + '')
-    }
-    else {
-        res.push(`${start}->${last}`)
+        const item = this.#assist.pop()!
+        for (let i = this.#assist.length - 1; i >= 0; i--) {
+            this.#store.push(this.#assist.pop()!)
+        }
+        return item
     }
 
-    return res
+    peek(): number {
+        for (let i = this.#store.length - 1; i >= 0; i--) {
+            this.#assist.push(this.#store.pop()!)
+        }
+        const item = this.#assist[this.#assist.length - 1]
+        for (let i = this.#assist.length - 1; i >= 0; i--) {
+            this.#store.push(this.#assist.pop()!)
+        }
+        return item
+    }
+
+    empty(): boolean {
+        return this.#store.length === 0
+    }
 }
 
+const aa = new MyQueue()
+aa.push(1)
+console.log(aa.pop())
+console.log(aa.empty())
+
+// function lk(nums: number[]): string[] {
+//     if(nums.length === 0) return []
+//
+//     const res: string[] = []
+//
+//     // 从第一个开始直接计数
+//     let start: number = nums[0]
+//
+//     const last = nums.reduce((prev, curr) => {
+//         // 已经开始计数 且遇到终点
+//         if(curr - prev !== 1) {
+//             // 只计了一个数
+//             if(prev === start) {
+//                 res.push(start + '')
+//             }
+//             // 计了多个数
+//             else {
+//                 res.push(`${start}->${prev}`)
+//             }
+//             start = curr
+//         }
+//         return curr
+//     })
+//
+//     if(start === last) {
+//         res.push(start + '')
+//     }
+//     else {
+//         res.push(`${start}->${last}`)
+//     }
+//
+//     return res
+// }
+
 export {
-    lk
+    // lk
 }
 
 
