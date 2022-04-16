ff69927386fe4a444ef0b70fea958d3404387eb6
lopo <lopo@zju.edu.cn>
2022-04-16 21:32:12 +0800

    daily: 2022-04-15 #268

--- src/lk.ts
+++ src/lk.ts
@@ -1,12 +1,12 @@
-// class ListNode {
-//     val: number
-//     next: ListNode | null
-//
-//     constructor(val?: number, next?: ListNode | null) {
-//         this.val = (val === undefined ? 0 : val)
-//         this.next = (next === undefined ? null : next)
-//     }
-// }
+class ListNode {
+    val: number
+    next: ListNode | null
+
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
 
 // class TreeNode {
 //     val: number
@@ -20,118 +20,24 @@
 //     }
 // }
 
-// class NestedInteger {
-//     #list: (number | NestedInteger) [] = []
-//
-//     constructor(val?: number) {
-//         if(val !== undefined) this.#list.push(val)
-//     }
-//
-//     isInteger() {
-//         return this.#list.length === 1 && typeof this.#list[1] === 'number'
-//     }
-//
-//     getInteger() {
-//         return this.isInteger() ? this.#list[0] : null
-//     }
-//
-//     setInteger(val: number) {
-//         this.#list = [ val ]
-//     }
-//
-//     add(elem: NestedInteger) {
-//         this.#list.push(elem)
-//     }
-//
-//     getList(): NestedInteger[] {
-//         return this.isInteger()
-//             ? []
-//             : this.#list.filter((item) => {
-//                 return item instanceof NestedInteger
-//             }) as NestedInteger[]
-//     }
-// }
 
-class MyQueue {
-    #store: number[] = []
-    #assist: number[] = []
+function lk(nums: number[]): number {
+    let remain = nums.length * (nums.length + 1) / 2
 
-    constructor() {
-
+    for(let i = 0; i < nums.length; i ++) {
+        remain = remain - nums[i]
     }
 
-    push(x: number): void {
-        this.#store.push(x)
-    }
-
-    pop(): number {
-        for (let i = this.#store.length - 1; i >= 0; i--) {
-            this.#assist.push(this.#store.pop()!)
-        }
-        const item = this.#assist.pop()!
-        for (let i = this.#assist.length - 1; i >= 0; i--) {
-            this.#store.push(this.#assist.pop()!)
-        }
-        return item
-    }
-
-    peek(): number {
-        for (let i = this.#store.length - 1; i >= 0; i--) {
-            this.#assist.push(this.#store.pop()!)
-        }
-        const item = this.#assist[this.#assist.length - 1]
-        for (let i = this.#assist.length - 1; i >= 0; i--) {
-            this.#store.push(this.#assist.pop()!)
-        }
-        return item
-    }
-
-    empty(): boolean {
-        return this.#store.length === 0
-    }
+    return remain
 }
-
-const aa = new MyQueue()
-aa.push(1)
-console.log(aa.pop())
-console.log(aa.empty())
-
-// function lk(nums: number[]): string[] {
-//     if(nums.length === 0) return []
-//
-//     const res: string[] = []
-//
-//     // 从第一个开始直接计数
-//     let start: number = nums[0]
-//
-//     const last = nums.reduce((prev, curr) => {
-//         // 已经开始计数 且遇到终点
-//         if(curr - prev !== 1) {
-//             // 只计了一个数
-//             if(prev === start) {
-//                 res.push(start + '')
-//             }
-//             // 计了多个数
-//             else {
-//                 res.push(`${start}->${prev}`)
-//             }
-//             start = curr
-//         }
-//         return curr
-//     })
-//
-//     if(start === last) {
-//         res.push(start + '')
-//     }
-//     else {
-//         res.push(`${start}->${last}`)
-//     }
-//
-//     return res
-// }
 
 export {
     // lk
 }
 
+const head = new ListNode(1)
+head.next = new ListNode(2)
+head.next.next = new ListNode(2)
+head.next.next.next = new ListNode(1)
 
+console.log(lk(head))
