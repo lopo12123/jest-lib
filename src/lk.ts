973bbc8e51ba06a1bd349789aeb451c50f046b19
lopo <lopo@zju.edu.cn>
2022-04-25 09:42:56 +0800

    daily: #398 hash

--- src/lk.ts
+++ src/lk.ts
@@ -20,23 +20,43 @@
 //     }
 // }
 
-function lk(s: string, t: string): boolean {
-    if(s === '') return true
-    else if(s.length > t.length) return false
-    else {
-        let p_s = 0
-        for (let i = 0; i < t.length; i++) {
-            // if(p_s === s.length - 1) return true
-            if(s[p_s] === t[i]) p_s++
-            console.log(t[i], p_s)
+// function lk(s: string, t: string): boolean {
+//     if(s === '') return true
+//     else if(s.length > t.length) return false
+//     else {
+//         let p_s = 0
+//         for (let i = 0; i < t.length; i++) {
+//             // if(p_s === s.length - 1) return true
+//             if(s[p_s] === t[i]) p_s++
+//             console.log(t[i], p_s)
+//         }
+//         return p_s === s.length
+//     }
+//
+// }
+
+class Solution {
+    #hash = new Map<number, number[]>()
+
+    constructor(nums: number[]) {
+        for (let i = 0; i < nums.length; i++) {
+            const ori = this.#hash.get(nums[i])
+            if(ori === undefined) {
+                this.#hash.set(nums[i], [ i ])
+            }
+            else {
+                this.#hash.set(nums[i], [...ori, i])
+            }
         }
-        return p_s === s.length
     }
 
+    pick(target: number): number {
+        const allIndex = this.#hash.get(target) as number[]
+
+        return allIndex[~~(Math.random() * allIndex.length)]
+    }
 }
 
 export {
     // lk
 }
-
-console.log(lk('abc', 'ahbgdc'))
