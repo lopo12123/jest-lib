ab678822169ff6665fd37c44a76a9c8f787e756b
chenmh <chenmh@sucsoft.com>
2022-04-27 18:04:39 +0800

    #9 字符串比对

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,18 @@
 fn main() {
-    println!("find: {:#?}", lk(vec![3,3], 6));
+    // println!("find: {:#?}", lk(vec![3,3], 6));
+    println!("{}", lk(123321));
 }
 
-fn lk(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    use std::collections::HashMap;
-    use std::convert::TryInto;
-
-    fn usize_to_i32(val: impl TryInto<i32>) -> i32 {
-        let val = match val.try_into() {
-            Ok(v) => v,
-            Err(_) => panic!("can not do trans"),
-        };
-        return val;
-    }
-
-    // value: index
-    let mut visited: HashMap<i32, i32> = HashMap::new();
-
-    let len = nums.len();
-    let mut i = 0;
-    while i < len {
-        if visited.contains_key(&(target - nums[i])) {
-            return vec![*visited.get(&(target - nums[i])).unwrap(), usize_to_i32(i)];
-        } else {
-            visited.insert(nums[i], usize_to_i32(i));
+fn lk(nums: i32) -> bool {
+    let str: String = nums.to_string();
+    let l: usize = str.len();
+    let mut i: usize = 0;
+    while i < (l / 2) {
+        if str[i..(i + 1)] != str[(l - 1 - i)..(l - i)] {
+            return false;
         }
-
         i += 1;
     }
 
-    return vec![0, 0];
+    return true;
 }
