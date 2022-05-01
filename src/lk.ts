9a0a1cedb070c776b8c8cf36ae2a2fe910325201
lopo <lopo@zju.edu.cn>
2022-05-01 21:52:47 +0800

    #520

--- src/lk.ts
+++ src/lk.ts
@@ -38,14 +38,8 @@ class TreeNode {
 //     }
 // }
 
-function lk(n: number): number {
-    const fb = (x: number): number => {
-        if(x === 0) return 0
-        else if(x === 1) return 1
-        else return fb(x - 1) + fb(x - 2)
-    }
-
-    return fb(n)
+function lk(word: string): boolean {
+    return word.toUpperCase() === word || word.toLowerCase() === word || word.slice(1).toLowerCase() === word.slice(1)
 }
 
 export {
