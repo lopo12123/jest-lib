7b27c24374242487f0367e8f1214fc9c87d83842
lopo <lopo@zju.edu.cn>
2022-05-02 22:02:41 +0800

    #557

--- src/lk.ts
+++ src/lk.ts
@@ -38,71 +38,9 @@
 //     }
 // }
 
-function lk(code: string): boolean {
-    // 不是标签包围的直径返回false
-    if(code[0] !== '<' && code[code.length - 1] !== '>') return false
-
-    const code_without_cdata = code.replace(/<!\[(cdata|CDATA)\[[\s\S]*?]]>/g, '-')
-
-    const tags: string[] = code_without_cdata.match(/<\/?[A-Z]*>/g) ?? []
-    const tag_num = tags.length
-
-    // console.log('no cdata: ', code_without_cdata)
-    // console.log('tags: ', tags)
-
-    // 奇数tag不能成对
-    if(tag_num % 2 === 1 || tag_num < 2) return false
-    // 最外层标签不匹配或不在最外围
-    if(
-        tags[0].toUpperCase() !== tags[0]
-        || tags[0].slice(1) !== tags[tag_num - 1].slice(2)
-        || code.slice(0, tags[0].length) !== tags[0]
-        || code.slice(code.length - tags[tag_num - 1].length) !== tags[tag_num - 1]
-    ) return false
-
-    const tag_stack: string[] = []
-    for (let i = 0; i < tag_num; i++) {
-        // 标签非大写字母
-        if(tags[i].toUpperCase() !== tags[i]) {
-            // console.log('not upper case: ', tags[i], i)
-            return false
-        }
-
-        // 左标签
-        if(tags[i][1] !== '/') {
-            // 标签过长过短
-            if(tags[i].length > 11 || tags[i].length < 3) return false
-            tag_stack.unshift(tags[i].slice(1))
-        }
-        // 右标签
-        else {
-            // 标签过长过短
-            if(tags[i].length > 12 || tags[i].length < 3) return false
-            if(tag_stack[0] !== tags[i].slice(2)) {
-                // console.log(tags[i], i)
-                return false
-            }
-            else tag_stack.shift()
-        }
-    }
-
-    if(tag_stack.length !== 0) return false
-
-    const code_without_tag = code_without_cdata.replace(/<\/?[A-Z]*>/g, '')
-
-    // console.log('no tag: ', code_without_tag)
-
-    return !code_without_tag.includes('<')
+function lk(s: string): string {
+    return s.split(' ').map(x => x.split('').reverse().join('')).join(' ')
 }
-
-
-// console.log(lk("<DIV>This is the first line <![CDATA[<div> <![cdata]> [[]]</div>   ]]>  <DIV> <A>  <![CDATA[<b>]]>  </A>  <A> <C></C></A></DIV>    </DIV>"))
-// console.log(lk("<A></A><B></B>"))
-// console.log(lk("<A></A>>"))
-console.log(lk("<DIV><YFSYYS><UVBNIQ><XPMXUNT><WNGMV><OJJGQREMT><Z><GEJDP><LIQS><NCVYU><RAS><UYFKCJCDN><NA><POJVYT><Z><TDC><VUIZQC><BNANGX><TOF><MR>MK</MR></TOF></BNANGX></VUIZQC></TDC></Z></POJVYT></NA></UYFKCJCDN></RAS></NCVYU></LIQS></GEJDP></Z></OJJGQREMT></WNGMV></XPMXUNT></UVBNIQ></YFSYYS></DIV>"))
-// console.log(lk("<A><A>456</A>  <A> 123  !!  <![CDATA[]]>  123 </A>   <A>123</A></A>"))
-// console.log(lk("<DIV>This is the first line <![CDATA[<div>]]></DIV>"))
-// console.log(lk("<DIV>>>  ![cdata[]] <![CDATA[<div>]>]]>]]>>]</DIV>"))
 
 export {
     lk
