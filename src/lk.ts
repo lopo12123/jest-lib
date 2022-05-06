f1cd1dc07700798d8f7986718fba43881a4a832f
lopo <lopo@zju.edu.cn>
2022-05-06 18:01:42 +0800

    #703

--- src/lk.ts
+++ src/lk.ts
@@ -61,20 +61,63 @@ function lk(bits: number[]): boolean {
     return len % 2 === 0
 }
 
-class RecentCounter {
-    #queue: number[] = []
-    constructor() {
+class KthLargest {
+    #k: number
+    #heap: number[] = [] // 小 - 大
 
+    constructor(k: number, nums: number[]) {
+        this.#k = k
+        this.#heap = nums.sort((a, b) => a - b).slice(-k)
     }
 
-    ping(t: number): number {
-        this.#queue.push(t)
-        while (t - this.#queue[0] > 3000) {
-            this.#queue.shift()
+    add(val: number): number {
+        if(this.#heap.length < this.#k) {
+            let p = this.#heap.length
+            this.#heap[p] = val
+
+            while (this.#heap[Math.floor((p - 1) / 2)] > val && p > 0) {
+                this.#heap[p] = this.#heap[Math.floor((p - 1) / 2)]
+                this.#heap[Math.floor((p - 1) / 2)] = val
+                p = Math.floor((p - 1) / 2)
+            }
+
+            return this.#heap[0]
         }
-        return this.#queue.length
+        else {
+            if(val <= this.#heap[0]) return this.#heap[0]
+
+            let p = 0
+            this.#heap[p] = val
+
+            while ((p * 2 + 1) < this.#k) {
+                const diveTo = this.#heap[p * 2 + 1] < (this.#heap[p * 2 + 2] ?? (this.#heap[p * 2 + 1] + 1))
+                    ? (p * 2 + 1) : (p * 2 + 2)
+                if(this.#heap[diveTo] < val) {
+                    this.#heap[p] = this.#heap[diveTo]
+                    this.#heap[diveTo] = val
+                    p = diveTo
+                }
+                else break
+            }
+
+            return this.#heap[0]
+        }
     }
 }
+
+const p = new KthLargest(3, [ 4, 5, 8, 2 ])
+console.log(p.add(3))  // 4 [1,3,3,5]
+console.log(p.add(5))  // 5 [3,3,5,5]
+console.log(p.add(10)) // 5 [3,5,5,7]
+console.log(p.add(9))  // 8 [4,5,5,7]
+console.log(p.add(4))  // 8 [4,5,5,7]
+
+// const p = new KthLargest(3, [])
+// console.log(p.add(-3))  // -3 [-3]
+// console.log(p.add(-2))  // -3 [-3, -2]
+// console.log(p.add(-4)) // -4 [-4, -3, -2]
+// console.log(p.add(0))  // -3 [-3, -2, 0]
+// console.log(p.add(4))  // -2 [-2, 0, 4]
 
 export {
     lk
