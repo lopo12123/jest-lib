734ca9812a16e7e9a6d9cc7530b99cecae78c649
lopo <lopo@zju.edu.cn>
2022-05-07 22:09:00 +0800

    #748

--- src/lk.ts
+++ src/lk.ts
@@ -48,56 +48,41 @@
 //     }
 // }
 
-function lk(start: string, end: string, bank: string[]): number {
-    const diff = (a: string, b: string) => {
-        if(a.length !== b.length) return -1
+function lk(licensePlate: string, words: string[]): string {
+    const oriCount: number[] = new Array(26).fill(0)
 
-        let count = 0
-        for (let i = 0; i < a.length; i++) {
-            if(a[i] !== b[i]) count += 1
+    for (let i = 0; i < licensePlate.length; i++) {
+        if(/[a-zA-Z]/.test(licensePlate[i])) {
+            oriCount[licensePlate[i].toLowerCase().charCodeAt(0) - 97] += 1
         }
-        return count
     }
 
-    const bankSet = new Set(bank)
+    let minWord = ''
 
-    // 如果end是无效的直接返回-1
-    if(!bankSet.has(end)) return -1
-
-    // 将起始串加入set, 开始构建树
-    bankSet.add(start)
-
-    const pool: (string | null)[] = [ end ]
-    let mutCount = 0
-    let layerEnd: boolean = true
-
-    while (bankSet.size > 0 && pool.length > 0) {
-        if(pool[0] === null) {
-            layerEnd = true
-            mutCount += 1
-            pool.shift()
-            continue
+    for (let j = 0; j < words.length; j++) {
+        const wordCount: number[] = new Array(26).fill(0)
+        for (let k = 0; k < words[j].length; k++) {
+            wordCount[words[j][k].toLowerCase().charCodeAt(0) - 97] += 1
         }
 
-        if(layerEnd) {
-            layerEnd = false
-            pool.push(null)
-        }
-
-        for (let str of bankSet) {
-            if(diff(str, pool[0]) === 1) {
-                if(str === start) return mutCount + 1
-                else {
-                    pool.push(str)
-                    bankSet.delete(str)
-                }
+        let satisfy = true
+        for (let comp = 0; comp < 26; comp++) {
+            if(wordCount[comp] < oriCount[comp]) {
+                satisfy = false
+                break
             }
         }
-        pool.shift()
+
+        if(satisfy && (minWord === '' || words[j].length < minWord.length)) {
+            minWord = words[j]
+        }
     }
 
-    return -1
+    return minWord
 }
+
+console.log(lk("1s3 PSt",
+    ["step","steps","stripe","stepple"]))
 
 export {
     lk
