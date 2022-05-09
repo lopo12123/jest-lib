fdc3f9144e2707b50d0be897dcbb5faf4cae7e90
lopo <lopo@zju.edu.cn>
2022-05-09 13:27:33 +0800

    #589

--- src/lk.ts
+++ src/lk.ts
@@ -38,39 +38,54 @@
 //     }
 // }
 
-// class Node {
-//     val: number
-//     children: Node[]
-//
-//     constructor(val?: number, children?: Node[]) {
-//         this.val = (val === undefined ? 0 : val)
-//         this.children = (children === undefined ? [] : children)
-//     }
-// }
+class Node {
+    val: number
+    children: Node[]
 
-function lk(s: string): number[] {
-    // D [0,d_count - 1] 降序； I [d_count + 1,n] 增序;
-    // res[0] === d_count
-    const n = s.length
-    const res: number[] = []
-
-    // const d_count = (s.match(/[D]/g) ?? []).join('').length
-    let d_count = 0
-    for (let i = 0; i < n; i ++) {
-        if(s[i] === 'D') d_count += 1
+    constructor(val?: number, children?: Node[]) {
+        this.val = (val === undefined ? 0 : val)
+        this.children = (children === undefined ? [] : children)
     }
-
-    res[0] = d_count
-
-    let curr_i = d_count + 1, curr_d = d_count - 1
-
-    for (let i = 0; i < n; i++) {
-        if(s[i] === 'I') res.push(curr_i++)
-        else res.push(curr_d--)
-    }
-
-    return res
 }
+
+function lk(root: Node | null): number[] {
+    const can: number[] = []
+
+    const root_first = (sub: Node | null) => {
+        if(sub === null) return
+        else {
+            can.push(sub.val)
+            sub.children.forEach(sub_root => {
+                root_first(sub_root)
+            })
+        }
+    }
+
+    root_first(root)
+
+    return can
+}
+
+const root = new Node(1)
+let sub1 = new Node(2)
+sub1.children = [
+    new Node(3),
+    new Node(4),
+]
+
+let sub2 = new Node(5)
+sub2.children = [
+    new Node(6),
+    new Node(7),
+]
+root.children = [
+    sub1,
+    sub2,
+    new Node(8),
+    new Node(9)
+]
+
+console.log(lk(root))
 
 export {
     lk
