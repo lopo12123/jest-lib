a8e295b6255b8a3277c52432bd14384ef33304ca
lopo <lopo@zju.edu.cn>
2022-05-11 17:49:28 +0800

    #1047

--- src/lk.ts
+++ src/lk.ts
@@ -54,115 +54,20 @@ const showTime = (fn: () => void) => {
     console.timeEnd('fn')
 }
 
-function lk(rows: number, cols: number, rCenter: number, cCenter: number): number[][] {
-    const res: [ number, number ][][] = []
+function lk(s: string): string {
+    const stack: string[] = []
 
-    let dis = 0
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
-            dis = Math.abs(r - rCenter) + Math.abs(c - cCenter)
-            if(!res[dis]) res[dis] = [ [ r, c ] ]
-            else res[dis].push([ r, c ])
-        }
+    for (let i = 0; i < s.length; i++) {
+        if(stack.at(-1) === s[i]) stack.pop()
+        else stack.push(s[i])
     }
 
-    return res.flat()
-}
-
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
-
-function serialize(root: TreeNode | null): string {
-    if(root === null) return ''
-
-    let s = ''
-
-    const dfs = (sub: TreeNode | null) => {
-        if(sub === null) s += ','
-        else {
-            s += sub.val + ','
-            dfs(sub.left)
-            dfs(sub.right)
-        }
-    }
-    dfs(root)
-
-    return s
-}
-
-function deserialize(data: string): TreeNode | null {
-    if(data === '') return null
-
-    const vals = data.split(',')
-
-    const root = new TreeNode(parseInt(vals[0]))
-    let p = 1
-
-    const dfs = (parent: TreeNode | null) => {
-        if(p >= vals.length || parent === null) return
-
-        parent.left = vals[p] === '' ? null : new TreeNode(parseInt(vals[p]))
-        p += 1
-        dfs(parent.left)
-
-        parent.right = vals[p] === '' ? null : new TreeNode(parseInt(vals[p]))
-        p += 1
-        dfs(parent.right)
-    }
-
-    dfs(root)
-
-    return root
+    return stack.join('')
 }
 
 // showTime(() => {
 //     // console.log(lk([ 'daeabc', 'aaeb', 'abacdc' ]))
 // })
-
-let root: TreeNode = {
-    val: 1,
-    left: {
-        val: 2,
-        left: {
-            val: 3,
-            left: null,
-            right: null
-        },
-        right: null
-    },
-    right: {
-        val: 4,
-        left: {
-            val: 5,
-            left: {
-                val: 6,
-                left: null,
-                right: null
-            },
-            right: null
-        },
-        right: null
-    }
-}
-
-let s = serialize(root)
-let d = deserialize(s)
-
-console.log(s)
-console.log(d)
-console.log(JSON.stringify(root) === JSON.stringify(d))
 
 export {
     lk
