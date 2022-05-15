236e91acc1cd53e0f76291ae858402186d96719a
lopo <lopo@zju.edu.cn>
2022-05-15 21:22:23 +0800

    #lcp 18

--- src/lk.ts
+++ src/lk.ts
@@ -78,36 +78,27 @@ class TreeNode {
 //     }
 // }
 
-function lk(n: number, k: number): number {
-    const selects = (m: number) => {
-        if(m === 0) return 1
+function lk(staple: number[], drinks: number[], x: number): number {
+    staple.sort((a, b) => a - b)
+    drinks.sort((a, b) => a - b)
 
-        let all = 1
-        for (let i = n; i > m; i--) {
-            all *= i
+    if(staple[0] + drinks[0] > x) return 0
+
+    let choices = 0
+    let last_drink_idx = drinks.length - 1
+    for (let i = 0; i < staple.length; i ++) {
+
+        if(staple[i] + drinks[last_drink_idx] > x) {
+            last_drink_idx -= 1
+            i -= 1
         }
-        for (let j = (n - m); j > 1; j--) {
-            all /= j
+        else {
+            choices = (choices + last_drink_idx + 1) % 1000000007
         }
-        return all
     }
-    if(k === 0 || k === n * n) return 1
-    else {
-        let count = 0
-        for (let x = 0; x < n; x++) {
-            for (let y = 0; y < n; y++) {
-                if((n * x + n * y - x * y) === k) {
-                    if(x === n) count += selects(y)
-                    else if(y === n) count += selects(x)
-                    else count += selects(x) * selects(y)
-                }
-            }
-        }
-        return count
-    }
+
+    return choices
 }
-
-console.log(lk(2, 4))
 
 
 // const showTime = (fn: () => void) => {
