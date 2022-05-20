7ba8efab60ac49060533750bfae78c015d13facb
lopo <lopo@zju.edu.cn>
2022-05-20 17:21:05 +0800

    [uncheck] #1572

--- src/lk.ts
+++ src/lk.ts
@@ -78,20 +78,28 @@
 //     }
 // }
 
-function lk(arr: number[]): boolean {
-    let count = 0
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] % 2 === 0) count = 0
-        else if(count === 2) return true
-        else {
-            count += 1
-        }
+function lk(mat: number[][]): number {
+    const max = mat.length - 1
+    let sum = 0
+    for (let i = 0; i <= max; i++) {
+        sum += mat[i][i] + mat[i][max - i]
     }
-    return false
+
+    return sum - (mat[max / 2]?.[max / 2] ?? 0)
 }
 
-console.log(lk([ 2, 6, 4, 1 ]))
-console.log(lk([ 1, 2, 34, 3, 4, 5, 7, 23, 12 ]))
+console.log(lk([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]))  // 25
+
+console.log(lk([
+    [1,2,3,1],
+    [4,5,6,2],
+    [7,8,9,3],
+    [7,8,9,3],
+]))  // 40
 
 // const showTime = (fn: () => void) => {
 //     console.time('fn')
