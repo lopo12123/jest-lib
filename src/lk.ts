b25668282bbc8728328920b7ced14cb4cfc4c45b
lopo <lopo@zju.edu.cn>
2022-05-27 16:50:19 +0800

    [uncheck] #997

--- src/lk.ts
+++ src/lk.ts
@@ -78,19 +78,24 @@
 //     }
 // }
 
-function lk(sequence: string, word: string): number {
-    const all = sequence.match(new RegExp(`(${word}){1,}`, 'g'))
+function lk(n: number, trust: number[][]): number {
+    const trust_to = new Map()
+    const trust_from = new Map()
 
-    if(!all) return 0
-    else {
-        const len0 = word.length
-        return all.reduce((prev, curr) => {
-            return Math.max(prev, curr.length / len0)
-        }, 0)
+    for (let i = 0; i < trust.length; i++) {
+        trust_to.set(trust[i][0], (trust_to.get(trust[i][0]) ?? 0) + 1)
+        trust_from.set(trust[i][1], (trust_from.get(trust[i][1]) ?? 0) + 1)
     }
+
+    for (let i = 1; i <= n; i++) {
+        if(!trust_to.has(i) && trust_from.get(i) === n - 1) return i
+    }
+    return -1
 }
 
-console.log(lk('ababc', 'ab'))
+console.log(lk(2, [ [ 1, 2 ] ]))  // 2
+console.log(lk(3, [ [ 1, 3 ], [ 2, 3 ] ]))  // 3
+console.log(lk(3, [ [ 1, 3 ], [ 2, 3 ], [ 3, 1 ] ]))  // -1
 
 // const showTime = (fn: () => void) => {
 //     console.time('fn')
