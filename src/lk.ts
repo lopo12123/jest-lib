2bcaa378138bdefb6e648854434ac0dc3abbfbb6
lopo <lopo@zju.edu.cn>
2022-05-29 20:53:20 +0800

    #1941

--- src/lk.ts
+++ src/lk.ts
@@ -78,18 +78,22 @@
 //     }
 // }
 
-function lk(words: string[]): boolean {
-    const group = words.length
+function lk(s: string): boolean {
     const count = new Array(26).fill(0)
 
-    for (let i = 0; i < group; i++) {
-        for (let j = 0; j < words[i].length; j++) {
-            count[words[i].charCodeAt(j) - 97] += 1
-        }
+    for (let i = 0; i < s.length; i ++) {
+        count[s.charCodeAt(i) - 97] += 1
     }
 
-    for (let i = 0; i < 26; i++) {
-        if(count[i] % group !== 0) return false
+    let times = 0
+
+    for (let i = 0; i < 26; i ++) {
+        if(count[i] !== 0) {
+            if(times === 0) times = count[i]
+            else {
+                if(times !== count[i]) return false
+            }
+        }
     }
     return true
 }
