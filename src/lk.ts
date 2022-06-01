4bfdf1f08db4e19fb64d5cdbf1f94f4b0b3a5d3b
lopo <lopo@zju.edu.cn>
2022-06-01 11:24:08 +0800

    [uncheck] #8 api

--- src/lk.ts
+++ src/lk.ts
@@ -84,54 +84,9 @@ const showTime = (fn: () => void) => {
     console.timeEnd('fn')
 }
 
-function lk(matchsticks: number[]): boolean {
-    const len = matchsticks.length
-    const C = matchsticks.reduce((prev, curr) => prev + curr)
-
-    if(C % 4 !== 0) return false
-
-    const if_can_divide_2 = (arr: number[], sum: number) => {
-        const size = arr.length
-
-        for (let i = 0; i < 2 ** size; i++) {
-            let curr = 0
-            let select = i
-            while (select > 0) {
-                curr += arr[select & 0b1]
-                select >>= 1
-            }
-            if(curr === sum / 2) return true
-        }
-        return false
-    }
-
-    for (let i = 0; i < 2 ** len; i++) {
-        let select = i, idx = 0
-        const part: number[][] = [ [], [] ]
-        let sum = [ 0, 0 ]
-        while (idx < len) {
-            part[select & 0b1].push(matchsticks[idx])
-            sum[select & 0b1] += matchsticks[idx]
-            select >>= 1
-            idx += 1
-        }
-
-        if(sum[0] === sum[1]) {
-            if(if_can_divide_2(part[0], sum[0]) && if_can_divide_2(part[1], sum[1])) return true
-        }
-    }
-
-    return false
+function lk(s: string): number {
+    return parseInt(s)
 }
-
-// 1234567
-// 7 1 2 4
-// 6 3 5
-
-console.log(lk([ 1, 1, 2, 2, 2 ]))  // true
-console.log(lk([ 3, 3, 3, 3, 4 ]))  // false
-console.log(lk([ 1, 2, 3, 4, 5, 6, 7 ]))  // true
-console.log(lk([ 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6 ]))  // true
 
 export {
     lk
