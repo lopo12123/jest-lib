fc37ee201bbd7fa13e79e164969cff2e57c632d3
lopo <lopo@zju.edu.cn>
2022-06-08 14:55:41 +0800

    #1037

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,23 @@
-fn main() {
-    println!("result: {:?}", lk("IDIIDDIDDDIIII"));
-    println!("result: {:?}", lk("IIDIDIDI"));
-    println!("result: {:?}", lk("IDIDIDDDDDIIDIIDD"));
-    println!("result: {:?}", lk("DDIIIIIDIDDD"));
-    println!("result: {:?}", lk("IDII"));
+struct Solution {}
+
+impl Solution {
+    pub fn is_boomerang(points: Vec<Vec<i32>>) -> bool {
+        if points[0][0] == points[1][0] && points[0][1] == points[1][1]
+            || points[0][0] == points[2][0] && points[0][1] == points[2][1]
+            || points[1][0] == points[2][0] && points[1][1] == points[2][1]
+            || points[0][0] == points[1][0] && points[0][0] == points[2][0]
+            || points[0][1] == points[1][1] && points[0][1] == points[2][1]
+        {
+            false
+        } else {
+            (points[0][1] - points[1][1]) * (points[0][0] - points[2][0])
+                != (points[0][1] - points[2][1]) * (points[0][0] - points[1][0])
+        }
+    }
 }
 
-fn lk(s: &str) -> Vec<i32> {
-    let s_len = s.len();
+fn main() {
+    let res = Solution::is_boomerang(vec![vec![1, 2], vec![1, 3], vec![2, 2]]);
 
-    let mut res: Vec<i32> = vec![];
-    let mut d_count = 0;
-
-    // 统计 D 的数量
-    let mut i = 0;
-    while i < s_len {
-        if let "D" = &s[i..(i + 1)] {
-            d_count += 1
-        }
-
-        i += 1;
-    }
-
-    res.push(d_count);
-
-    let mut curr_i = d_count + 1;
-    let mut curr_d = d_count - 1;
-
-    i = 0;
-    while i < s_len {
-        if &s[i..(i + 1)] == "D" {
-            res.push(curr_d);
-            curr_d -= 1;
-        } else {
-            res.push(curr_i);
-            curr_i += 1;
-        }
-
-        i += 1;
-    }
-
-    return res;
+    println!("res is: {:?}", res);
 }
