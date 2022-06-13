bca24df94d46a1eafcc6e1fad9759527c9081695
lopo <lopo@zju.edu.cn>
2022-06-13 22:07:00 +0800

    #9

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
 struct Solution {}
 
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut set: std::collections::HashMap<i32, usize> = std::collections::HashMap::new();
-
-        let mut i = 0;
-        while i < nums.len() {
-            if set.contains_key(&nums[i]) {
-                return vec![set.get(&nums[i]).unwrap().clone() as i32, i as i32];
-            }
-
-            set.insert(target - nums[i], i);
-
-            i += 1;
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false;
+        }
+        if x == 0 {
+            return true;
         }
 
-        return vec![0, 0];
+        let mut xx = x;
+        let mut bits = vec![];
+        while xx > 0 {
+            bits.push(xx % 10);
+            xx = (xx - xx % 10) / 10;
+        }
+
+        let mut l = 0;
+        let mut r = bits.len() - 1;
+
+        while l < r {
+            if bits[l] != bits[r] {
+                return false;
+            } else {
+                l += 1;
+                r -= 1;
+            }
+        }
+
+        true
     }
 }
 
-fn main() {
-    let res1 = Solution::two_sum(vec![2, 7, 11, 15], 9);
-    let res2 = Solution::two_sum(vec![3, 2, 4], 6);
-    let res3 = Solution::two_sum(vec![3, 3], 6);
-
-    println!("res1 is: {:?}", res1);  // [0, 1]
-    println!("res2 is: {:?}", res2);  // [1, 2]
-    println!("res3 is: {:?}", res3);  // [0, 1]
-}
+fn main() {}
