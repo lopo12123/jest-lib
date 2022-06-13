a04ef2d79e3b15db66d86514aa771a7aacf66f92
lopo <lopo@zju.edu.cn>
2022-06-13 22:00:45 +0800

    #1

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
 struct Solution {}
 
 impl Solution {
-    pub fn height_checker(heights: Vec<i32>) -> i32 {
-        let in_order = &mut heights.clone();
-        in_order.sort_unstable();
-        let mut count = 0;
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut set: std::collections::HashMap<i32, usize> = std::collections::HashMap::new();
 
         let mut i = 0;
-        while i < heights.len() {
-            if heights[i] != in_order[i] {
-                count += 1
+        while i < nums.len() {
+            if set.contains_key(&nums[i]) {
+                return vec![set.get(&nums[i]).unwrap().clone() as i32, i as i32];
             }
+
+            set.insert(target - nums[i], i);
+
             i += 1;
         }
-        count
+
+        return vec![0, 0];
     }
 }
 
 fn main() {
-    let res1 = Solution::height_checker(vec![1, 1, 4, 2, 1, 3]);
-    let res2 = Solution::height_checker(vec![5, 1, 2, 3, 4]);
-    let res3 = Solution::height_checker(vec![1, 2, 3, 4, 5]);
+    let res1 = Solution::two_sum(vec![2, 7, 11, 15], 9);
+    let res2 = Solution::two_sum(vec![3, 2, 4], 6);
+    let res3 = Solution::two_sum(vec![3, 3], 6);
 
-    println!("res1 is: {:?}", res1);  // 3
-    println!("res2 is: {:?}", res2);  // 5
-    println!("res3 is: {:?}", res3);  // 0
+    println!("res1 is: {:?}", res1);  // [0, 1]
+    println!("res2 is: {:?}", res2);  // [1, 2]
+    println!("res3 is: {:?}", res3);  // [0, 1]
 }
