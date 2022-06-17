bbc24623ee14a405c4acb3b99870568cf35ec44d
lopo <lopo@zju.edu.cn>
2022-06-17 10:29:58 +0800

    #1089

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,34 @@
 struct Solution {}
 
 impl Solution {
-    pub fn find_diagonal_order(mat: Vec<Vec<i32>>) -> Vec<i32> {
-        let row = mat.len();
-        let col = mat[0].len();
+    pub fn duplicate_zeros(arr: &mut Vec<i32>) {
+        let pool: Vec<i32> = arr.clone();
 
-        return if row == 1 {
-            mat[0].clone()
-        } else if col == 1 {
-            let mut res = vec![];
-            for item in mat.iter() {
-                res.push(item[0])
-            }
-            res
-        } else {
-            let max = std::cmp::max(row, col) * 2;
-            let mut store = vec![];
+        let mut p = 0;
+        let mut t = 0;
+        while p < arr.len() {
+            if pool[t] == 0 {
+                arr[p] = 0;
+                p += 1;
 
-            let mut xy = 0;
-            while xy <= max {
-                let mut offset = 0;
-                while offset <= xy {
-                    if xy % 2 == 0 && xy - offset < row && offset < col
-                        || xy % 2 != 0 && xy - offset < col && offset < row {
-                        store.push(
-                            if xy % 2 == 0 {
-                                mat[xy - offset][offset]
-                            } else {
-                                mat[offset][xy - offset]
-                            }
-                        );
-                    }
-                    offset += 1;
+                if p < arr.len() {
+                    arr[p] = 0;
+                    p += 1;
                 }
-                xy += 1;
+            } else {
+                arr[p] = pool[t];
+                p += 1;
             }
-
-            store
+            t += 1;
         }
     }
 }
 
 
 fn main() {
-    let a = Solution::find_diagonal_order(vec![
-        vec![1, 3],
-        vec![2, 4],
-        vec![5, 7],
-        vec![6, 8],
-    ]);
+    let mut arr = vec![1, 0, 2, 0, 3];
 
-    println!("{:?}", a);
+    Solution::duplicate_zeros(&mut arr);
+
+    println!("{:?}", arr);
 }
