8d7a34add917fc57e947b0bcb34e0d39702fb2c0
lopo <lopo@zju.edu.cn>
2022-06-19 21:20:11 +0800

    #508

--- src/lk.ts
+++ src/lk.ts
@@ -7,18 +7,18 @@
 //         this.next = (next === undefined ? null : next)
 //     }
 // }
-//
-// class TreeNode {
-//     val: number
-//     left: TreeNode | null
-//     right: TreeNode | null
-//
-//     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-//         this.val = (val === undefined ? 0 : val)
-//         this.left = (left === undefined ? null : left)
-//         this.right = (right === undefined ? null : right)
-//     }
-// }
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
 
 // class Node {
 //     val: boolean
@@ -88,25 +88,25 @@ class Node {
 //     }
 // }
 
-function lk(head: Node | null, insertVal: number): Node | null {
-    if(head === null) {
-        const p = new Node(insertVal);
-        p.next = p;
-        return p;
-    }
-    else {
-        let pt = head;
-        while (pt.next !== head) {
-            if(pt.val <= insertVal && pt.next!.val >= insertVal
-                || pt.val > pt.next!.val && (pt.val <= insertVal || pt.next!.val >= insertVal)) {
-                pt.next = new Node(insertVal, pt.next!);
-                return head;
-            }
-            pt = pt.next!;
+function lk(root: TreeNode): number[] {
+    const count = new Map<number, number>()
+
+    const dfs = (sub: TreeNode | null): number => {
+        if(sub === null) return 0
+        else {
+            const sum = sub.val + dfs(sub.left) + dfs(sub.right)
+            count.set(sum, (count.get(sum) ?? 0) + 1)
+            return sum
         }
-        pt.next = new Node(insertVal, head);
-        return head;
     }
+
+    dfs(root)
+
+    const sorted = [ ...count ].sort((a, b) => b[1] - a[1])
+    for (let i = 1; i < sorted.length; i++) {
+        if(sorted[i][1] !== sorted[i - 1][1]) return sorted.slice(0, i).map(x => x[0])
+    }
+    return sorted.map(x => x[0])
 }
 
 // console.log(lk([
