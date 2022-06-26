19fb2e194060a64b1afc5e57d89e8aba47a38312
lopo <lopo@zju.edu.cn>
2022-06-26 21:29:00 +0800

    #710

--- src/lk.ts
+++ src/lk.ts
@@ -91,7 +91,7 @@ class TreeNode {
 function lk(costs: number[][]): number {
     let cost_till_now = costs[0]
 
-    for (let i = 1; i < costs.length; i ++) {
+    for (let i = 1; i < costs.length; i++) {
         cost_till_now = [
             Math.min(cost_till_now[1], cost_till_now[2]) + costs[i][0],
             Math.min(cost_till_now[0], cost_till_now[2]) + costs[i][1],
@@ -101,6 +101,46 @@ function lk(costs: number[][]): number {
 
     return Math.min(...cost_till_now)
 }
+
+class Solution {
+    #valid_max
+    #mapper = new Map()
+
+    constructor(n: number, blacklist: number[]) {
+        let valid_max = n - blacklist.length
+        this.#valid_max = valid_max
+
+        blacklist.sort((a, b) => a - b)
+        let map_to = n - 1
+        let p_tail = blacklist.length - 1
+        for (let i = 0; i < blacklist.length; i++) {
+            if(blacklist[i] >= valid_max) return
+            else {
+                while (blacklist[p_tail] === map_to) {
+                    map_to -= 1
+                    p_tail -= 1
+                }
+                this.#mapper.set(blacklist[i], map_to)
+                map_to -= 1
+            }
+        }
+    }
+
+    #last_pick = -1
+
+    pick(): number {
+        // if(this.#last_pick === -1) console.log(this.#valid_max, this.#mapper)
+        this.#last_pick = (this.#last_pick + 1) % this.#valid_max
+        return this.#mapper.get(this.#last_pick) ?? this.#last_pick
+    }
+}
+
+const aa = new Solution(4, [ 2, 1 ])
+console.log(aa.pick())
+console.log(aa.pick())
+console.log(aa.pick())
+console.log(aa.pick())
+// const aa = new Solution(1000000000, [654321654])
 
 export {
     lk
