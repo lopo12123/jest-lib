9833c85743a82c22fdaa6ae74d1edba03e637ea5
lopo <lopo@zju.edu.cn>
2022-06-28 17:41:33 +0800

    #324

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,43 @@
 struct Solution {}
 
 impl Solution {
-    pub fn defang_i_paddr(address: String) -> String {
-        let mut new_addr = String::from("");
+    pub fn wiggle_sort(nums: &mut Vec<i32>) {
+        let len = nums.len();
 
-        let mut start = 0;
-        let mut p = 0;
+        nums.sort_unstable_by(|a, b| b.cmp(a));
 
-        while p < address.len() {
-            if &address[p..p + 1] == "." {
-                new_addr.push_str(&address[start..p]);
-                new_addr.push_str("[.]");
-                start = p + 1;
+        let mut low = vec![];
+        let mut high = vec![];
+
+        let mut i = 0;
+        while i < len {
+            if i < len / 2 {
+                high.push(nums[i])
+            } else {
+                low.push(nums[i])
             }
+            i += 1;
+        }
+
+        let mut p = 0;
+        while p < high.len() {
+            nums[p * 2] = low[p];
+            nums[p * 2 + 1] = high[p];
+
             p += 1;
         }
-        new_addr.push_str(&address[start..p]);
 
-        new_addr
+        if len % 2 == 1 {
+            nums[len - 1] = low[(len - 1) / 2];
+        }
+
+        // println!("low: {:?}, \nhigh: {:?}", low, high);
     }
 }
 
 
 fn main() {
-    println!("{:?}", Solution::defang_i_paddr(String::from("1.1.1.1")));
+    let mut nums1 = vec![1,1,1,1,2,2,2,2];
+    Solution::wiggle_sort(&mut nums1);
+    println!("{:?}", nums1);
 }
