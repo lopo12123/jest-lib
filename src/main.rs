00ca5d6db63e4dd13db6f834fecaa998a2044053
lopo <lopo@zju.edu.cn>
2022-07-04 17:12:47 +0800

    #1200

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,38 @@
 struct Solution {}
 
 impl Solution {
-    pub fn wiggle_sort(nums: &mut Vec<i32>) {
-        let len = nums.len();
+    pub fn minimum_abs_difference(arr: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut arr = arr.clone();
+        arr.sort_unstable();
 
-        nums.sort_unstable_by(|a, b| b.cmp(a));
-
-        let mut low = vec![];
-        let mut high = vec![];
-
-        let mut i = 0;
-        while i < len {
-            if i < len / 2 {
-                high.push(nums[i])
-            } else {
-                low.push(nums[i])
+        let mut result: Vec<Vec<i32>> = vec![];
+        let mut min_abs = arr[1] - arr[0];
+        let mut p = 1;
+        while p < arr.len() {
+            match (arr[p] - arr[p - 1]).cmp(&min_abs) {
+                std::cmp::Ordering::Less => {
+                    result = vec![vec![arr[p - 1], arr[p]]];
+                    min_abs = arr[p] - arr[p - 1];
+                }
+                std::cmp::Ordering::Equal => {
+                    result.push(vec![arr[p - 1], arr[p]]);
+                }
+                std::cmp::Ordering::Greater => ()
             }
-            i += 1;
-        }
-
-        let mut p = 0;
-        while p < high.len() {
-            nums[p * 2] = low[p];
-            nums[p * 2 + 1] = high[p];
-
             p += 1;
         }
 
-        if len % 2 == 1 {
-            nums[len - 1] = low[(len - 1) / 2];
-        }
-
-        // println!("low: {:?}, \nhigh: {:?}", low, high);
+        result
     }
 }
 
 
 fn main() {
-    let mut nums1 = vec![1,1,1,1,2,2,2,2];
-    Solution::wiggle_sort(&mut nums1);
-    println!("{:?}", nums1);
+    let nums1 = vec![4, 3, 1, 2];  // [[1,2], [2,3], [3,4]]
+    let nums2 = vec![1, 3, 6, 10, 15];  // [[1,3]]
+    let nums3 = vec![3, 8, -10, 23, 19, -4, -14, 27];  // [[-14,-10],[19,23],[23,27]]
+
+    println!("{:?}", Solution::minimum_abs_difference(nums1));
+    println!("{:?}", Solution::minimum_abs_difference(nums2));
+    println!("{:?}", Solution::minimum_abs_difference(nums3));
 }
