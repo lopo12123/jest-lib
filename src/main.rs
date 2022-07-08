b8dacf036451b8919243abe12bccf4118824cae4
lopo <lopo@zju.edu.cn>
2022-07-08 14:17:08 +0800

    #1217

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,24 @@
 struct Solution {}
 
 impl Solution {
-    pub fn minimum_abs_difference(arr: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut arr = arr.clone();
-        arr.sort_unstable();
+    pub fn min_cost_to_move_chips(position: Vec<i32>) -> i32 {
+        let mut odd_even = [0, 0];
 
-        let mut result: Vec<Vec<i32>> = vec![];
-        let mut min_abs = arr[1] - arr[0];
-        let mut p = 1;
-        while p < arr.len() {
-            match (arr[p] - arr[p - 1]).cmp(&min_abs) {
-                std::cmp::Ordering::Less => {
-                    result = vec![vec![arr[p - 1], arr[p]]];
-                    min_abs = arr[p] - arr[p - 1];
-                }
-                std::cmp::Ordering::Equal => {
-                    result.push(vec![arr[p - 1], arr[p]]);
-                }
-                std::cmp::Ordering::Greater => ()
-            }
-            p += 1;
+        for po in position {
+            odd_even[(po % 2) as usize] += 1;
         }
 
-        result
+        std::cmp::min(odd_even[0], odd_even[1])
     }
 }
 
 
 fn main() {
-    let nums1 = vec![4, 3, 1, 2];  // [[1,2], [2,3], [3,4]]
-    let nums2 = vec![1, 3, 6, 10, 15];  // [[1,3]]
-    let nums3 = vec![3, 8, -10, 23, 19, -4, -14, 27];  // [[-14,-10],[19,23],[23,27]]
+    let nums1 = vec![1, 2, 3];  // 1
+    let nums2 = vec![2, 2, 2, 3, 3];  // 2
+    let nums3 = vec![1, 100000];  // 1
 
-    println!("{:?}", Solution::minimum_abs_difference(nums1));
-    println!("{:?}", Solution::minimum_abs_difference(nums2));
-    println!("{:?}", Solution::minimum_abs_difference(nums3));
+    println!("{:?}", Solution::min_cost_to_move_chips(nums1));
+    println!("{:?}", Solution::min_cost_to_move_chips(nums2));
+    println!("{:?}", Solution::min_cost_to_move_chips(nums3));
 }
