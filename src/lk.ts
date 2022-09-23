4374d4f074b2657a6c9a26611b602375e5a3e2c4
chenmh <chenmh@sucsoft.com>
2022-09-23 10:44:16 +0800

    [uncheck] #707

--- src/lk.ts
+++ src/lk.ts
@@ -8,17 +8,17 @@
 //     }
 // }
 
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.left = (left === undefined ? null : left)
-        this.right = (right === undefined ? null : right)
-    }
-}
+// class TreeNode {
+//     val: number
+//     left: TreeNode | null
+//     right: TreeNode | null
+//
+//     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//         this.val = (val === undefined ? 0 : val)
+//         this.left = (left === undefined ? null : left)
+//         this.right = (right === undefined ? null : right)
+//     }
+// }
 
 // class Node {
 //     val: boolean
@@ -88,10 +88,164 @@ class TreeNode {
 //     }
 // }
 
+/**
+ * @example
+ MyLinkedList linkedList = new MyLinkedList();
+ linkedList.addAtHead(1);
+ linkedList.addAtTail(3);
+ linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
+ linkedList.get(1);            //返回2
+ linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+ linkedList.get(1);            //返回3
+ */
+type ListNode = {
+    prev: ListNode | null
+    next: ListNode | null
+    val: number
+}
+
+class MyLinkedList {
+    #head: ListNode | null = null
+    #tail: ListNode | null = null
+    #count: number = 0
+
+    print() {
+        let arr: number[] = []
+        let p = this.#head
+        while (p !== null) {
+            arr.push(p.val)
+            p = p.next
+        }
+        console.log(arr)
+    }
+
+    constructor() {
+    }
+
+    #findNode(index: number) {
+        let _cursor: ListNode | null
+        if(index > this.#count / 2) {
+            _cursor = this.#tail
+            for (let i = this.#count - 1; i > index; i--) {
+                _cursor = _cursor!.prev
+            }
+        }
+        else {
+            _cursor = this.#head
+            for (let i = 0; i < index; i++) {
+                _cursor = _cursor!.next
+            }
+        }
+        return _cursor
+    }
+
+    get(index: number): number {
+        if(index < 0 || index >= this.#count) return -1
+
+        return this.#findNode(index)!.val
+    }
+
+    addAtHead(val: number): void {
+        const _newHead: ListNode = {
+            prev: null,
+            next: this.#head,
+            val
+        }
+
+        if(this.#count === 0) {
+            this.#head = _newHead
+            this.#tail = _newHead
+        }
+        else {
+            this.#head!.prev = _newHead
+            this.#head = _newHead
+        }
+
+        this.#count += 1
+    }
+
+    addAtTail(val: number): void {
+        const _newTail: ListNode = {
+            prev: this.#tail,
+            next: null,
+            val
+        }
+
+        if(this.#count === 0) {
+            this.#head = _newTail
+            this.#tail = _newTail
+        }
+        else {
+            this.#tail!.next = _newTail
+            this.#tail = _newTail
+        }
+
+        this.#count += 1
+    }
+
+    addAtIndex(index: number, val: number): void {
+        if(index <= 0) this.addAtHead(val)
+        else if(index === this.#count) this.addAtTail(val)
+        else if(index > this.#count) return;
+        else {
+            const _cursor = this.#findNode(index)!
+
+            const _newNode: ListNode = {
+                prev: _cursor!.prev,
+                next: _cursor,
+                val
+            }
+            _cursor.prev!.next = _newNode
+            _cursor.prev = _newNode
+        }
+    }
+
+    deleteAtIndex(index: number): void {
+        if(index < 0 || index >= this.#count) return
+        else {
+            const _cursor = this.#findNode(index)!
+            const _prev = _cursor.prev
+            const _next = _cursor.next
+
+            // 删完
+            if(!_prev && !_next) {
+                this.#head = null
+                this.#tail = null
+            }
+            // 删头
+            else if(!_prev) {
+                this.#head = _next
+                _next!.prev = null
+            }
+            // 删尾
+            else if(!_next) {
+                this.#tail = _prev
+                _prev.next = null
+            }
+            // 中间
+            else {
+                _prev.next = _next
+                _next.prev = _prev
+            }
+        }
+    }
+}
+
+let linkedList: MyLinkedList = new MyLinkedList();
+linkedList.addAtHead(1);
+linkedList.addAtTail(3);
+linkedList.addAtIndex(1, 2);   //链表变为1-> 2-> 3
+linkedList.print()
+console.log(linkedList.get(1));            //返回2
+linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+linkedList.print()
+console.log(linkedList.get(1));            //返回3
+linkedList.print()
+
 function lk(logs: string[]): number {
     return logs.reduce((prev, curr) => {
-        if (curr === './') return prev
-        else if (curr === '../') return Math.max(prev - 1, 0)
+        if(curr === './') return prev
+        else if(curr === '../') return Math.max(prev - 1, 0)
         else return prev + 1
     }, 0)
 }
