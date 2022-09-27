85d4a12ef80a4faae173a4cf0376d95acd99c331
lopo <lopo@zju.edu.cn>
2022-09-27 10:12:22 +0800

    #面试题01.02 (map)

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,32 @@
-use std::process::id;
-
 struct Solution {}
 
 impl Solution {
-    pub fn decrypt(code: Vec<i32>, k: i32) -> Vec<i32> {
-        let n = code.len();
-        if k == 0 {
-            return vec![0; n];
+    pub fn check_permutation(s1: String, s2: String) -> bool {
+        let len = s1.len();
+        let mut _map: std::collections::HashMap<&str, i32> = std::collections::HashMap::new();
+
+        if len != s2.len() {
+            return false;
         }
 
-        let k_abs = k.abs() as usize;
-        let mut window_value = code.get(0..k_abs).unwrap().iter().sum::<i32>();
-        let mut decoded = vec![window_value];
-
-        for i in 1..n {
-            window_value -= code[i - 1];
-            window_value += code[(i - 1 + k_abs) % n];
-            decoded.push(window_value);
+        for i in 0..len {
+            *_map.entry(&s1[i..i + 1]).or_insert(0) += 1;
+            *_map.entry(&s2[i..i + 1]).or_insert(0) -= 1;
         }
 
-        if k > 0 {
-            decoded.rotate_left(1);
-        } else {
-            decoded.rotate_right(k_abs);
+        for (_, v) in _map {
+            if v != 0 {
+                return false;
+            }
         }
-
-        decoded
+        true
     }
 }
 
 
 fn main() {
-    let a1 = Solution::decrypt(vec![5, 7, 1, 4], 3);
-    let a2 = Solution::decrypt(vec![1, 2, 3, 4], 0);
-    let a3 = Solution::decrypt(vec![2, 4, 9, 3], -2);
-
-    println!("{:?}", a1);
-    println!("{:?}", a2);
-    println!("{:?}", a3);
+    println!("{}", Solution::check_permutation(String::from("aa"), String::from("ac")));
+    println!("{}", Solution::check_permutation(String::from("caa"), String::from("aca")));
+    println!("{}", Solution::check_permutation(String::from("aca"), String::from("acc")));
+    println!("{}", Solution::check_permutation(String::from("acv"), String::from("vca")));
 }
