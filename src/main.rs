5db043ce49ffe4bb358dd3755feba62178262763
lopo <lopo@zju.edu.cn>
2022-09-29 10:25:08 +0800

    #面试题01.09

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,19 @@
+use std::ops::Add;
+
 struct Solution {}
 
 impl Solution {
-    pub fn get_kth_magic_number(k: i32) -> i32 {
-        use std::collections::{BinaryHeap};
-        use std::cmp::Reverse;
-        let mut heap: BinaryHeap<Reverse<i32>> = BinaryHeap::from([Reverse(1)]);
+    pub fn is_fliped_string(s1: String, s2: String) -> bool {
+        if s1.len() != s2.len() { return false; }
 
-        let mut prev: i32 = -1;
-        let mut p: i32 = 0;
-        while p < k {
-            match heap.pop() {
-                Some(Reverse(top)) => {
-                    if top != prev {
-                        prev = top;
-
-                        if top < std::i32::MAX / 3 {
-                            heap.push(Reverse(top * 3));
-                        }
-                        if top < std::i32::MAX / 5 {
-                            heap.push(Reverse(top * 5));
-                        }
-                        if top < std::i32::MAX / 7 {
-                            heap.push(Reverse(top * 7));
-                        }
-                        p += 1;
-                    }
-                }
-                _ => {}
-            }
-        }
-
-        prev
+        let ss = s1.clone() + s1.as_str();
+        return ss.find(&s2) != None;
     }
 }
 
 
 fn main() {
-    let num_k = Solution::get_kth_magic_number(643);  // 1937xxxx
-    println!("{}", num_k);
+    let r1 = Solution::is_fliped_string("122".to_string(), "211".to_string());
+
+    println!("{r1}");
 }
