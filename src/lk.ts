9aca20491a510c051ef176951eba3cfbe841517a
lopo <lopo@zju.edu.cn>
2022-10-07 12:52:36 +0800

    #1800. 最大升序子数组和

--- src/lk.ts
+++ src/lk.ts
@@ -88,40 +88,28 @@
 //     }
 // }
 
-function lk(s1: string, s2: string): boolean {
-    if(s1.length !== s2.length) return false
-    if(s1 === '' && s2 === '') return true
+function lk(nums: number[]): number {
+    if (nums.length === 1) return nums[0]
+    else {
+        let maxSum = 0
+        let currSum = nums[0]
 
-    let compare_part = 1
-    const len = s1.length
-    const idx_of_head: number[] = []
-    let p_double_string = 0, p_s2 = 0
+        nums.reduce((prev, curr) => {
+            if (prev < curr) currSum += curr
+            else {
+                maxSum = Math.max(maxSum, currSum)
+                currSum = curr
+            }
+            console.log(curr, currSum, maxSum)
+            return curr
+        })
+        maxSum = Math.max(maxSum, currSum)
 
-    while (p_double_string < len && compare_part <= 2) {
-        if(s1[p_double_string] === s2[0] && p_s2 !== 0) idx_of_head.push(p_double_string)
-
-        if(s1[p_double_string] === s2[p_s2]) {
-            p_double_string += 1
-            p_s2 += 1
-        }
-        else {
-            p_double_string = idx_of_head.shift() ?? (p_double_string + Math.max(p_s2, 1))
-            p_s2 = 0
-        }
-
-        if(p_s2 === len) return true
-
-        if(p_double_string >= len) {
-            compare_part += 1
-            p_double_string %= len
-        }
+        return maxSum
     }
-    return false
 }
 
-console.log(lk('abc', 'bca'))  // t
-console.log(lk('aba', 'aab'))  // t
-console.log(lk('aba', 'bab'))  // f
+console.log(lk([10,20,30,40,50]))
 
 export {
     lk
