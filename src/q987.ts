d8b127995a71f2730be0b0aff045ada3b32d13e9
lopo <lopo@zju.edu.cn>
2024-02-13 23:36:28 +0800

    feat: q987 -- better

--- src/q987.ts
+++ src/q987.ts
@@ -1,5 +1,3 @@
-import { MinPriorityQueue } from '@datastructures-js/priority-queue'
-
 class TreeNode {
     val: number
     left: TreeNode | null
@@ -13,51 +11,36 @@ class TreeNode {
 }
 
 function verticalTraversal(root: TreeNode | null): number[][] {
-    // 稀疏矩阵
-    const matrix = []
+    const nodes: [ x: number, y: number, v: number ][] = []
 
     const dfs = (node: TreeNode | null, x: number, y: number) => {
         if(!node) return
 
-        // 第一次遇到此列
-        if(!matrix[x]) matrix[x] = []
-
-        const prev = matrix[x][y]
-        if(prev === undefined) {
-            // 第一次遇到此格 -- 直接填充
-            matrix[x][y] = node.val
-        } else if(typeof prev === 'number') {
-            const queue = new MinPriorityQueue()
-            queue.enqueue(prev)
-            queue.enqueue(node.val)
-            matrix[x][y] = queue
-        } else {
-            prev.enqueue(node.val)
-        }
+        nodes.push([ x, y, node.val ])
         dfs(node.left, x - 1, y + 1)
         dfs(node.right, x + 1, y + 1)
     }
+    dfs(root, 0, 0)
 
-    // 由于节点最多为 1000, 此处偏移 500 避免插入到数组的负索引
-    dfs(root, 500, 0)
-
-    // 去除空隙
-    const re: number[][] = []
-    matrix.map(sparse_col => {
-        if(sparse_col === undefined) return
-
-        const col: number[] = []
-        sparse_col.forEach(cell => {
-            if(cell === undefined) return
-            if(typeof cell === 'number') col.push(cell)
-            else {
-                while (!cell.isEmpty()) {
-                    col.push(cell.dequeue().element)
-                }
-            }
-        })
-        re.push(col)
+    // 按照 x-y-v 优先级排序
+    nodes.sort(([ x1, y1, v1 ], [ x2, y2, v2 ]) => {
+        if(x1 !== x2) return x1 - x2
+        else if(y1 != y2) return y1 - y2
+        else return v1 - v2
     })
+
+    const re: number[][] = []
+    let prev_x: number = nodes[0][0]
+    let temp_arr: number[] = []
+    for (const [ x, _, v ] of nodes) {
+        if(x !== prev_x) {
+            re.push(temp_arr)
+            temp_arr = []
+            prev_x = x
+        }
+        temp_arr.push(v)
+    }
+    re.push(temp_arr)
 
     return re
 }
